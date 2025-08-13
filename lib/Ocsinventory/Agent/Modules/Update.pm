b80c1ef8b83b235def16fe05e423377a7c9cda88
Saad Abdul Majid <saad.majid@7vals.com>
2025-08-13 06:38:48 -0700

    Download new installer

--- lib/Ocsinventory/Agent/Modules/Update.pm
+++ lib/Ocsinventory/Agent/Modules/Update.pm
@@ -10,8 +10,11 @@ package Ocsinventory::Agent::Modules::Update;
 use strict;
 use warnings;
 
+use version;
+use Fcntl qw/:flock/;
 use File::Path qw(make_path);
 use XML::Simple;
+use Digest::MD5;
 
 sub new {
     my $name = "update";
@@ -27,12 +30,12 @@ sub new {
     $self->{context} = $context;
 
     $self->{structure} = {
-        name => $name,
-        start_handler => undef,
-        prolog_writer => undef,
-        prolog_reader => $name . "_prolog_reader",
+        name              => $name,
+        start_handler     => undef,
+        prolog_writer     => undef,
+        prolog_reader     => $name . "_prolog_reader",
         inventory_handler => undef,
-        end_handler => undef,
+        end_handler       => $name . "_end_handler",
     };
 
     bless $self;
@@ -66,22 +69,10 @@ sub _write_update_info_xml {
     my ($self, $update_href) = @_;
     my $logger  = $self->{logger};
     my $context = $self->{context};
-    my $config  = $context->{config};
-
-    # Compute output dir under install path
-    my $base_dir  = $context->{installpath} || $config->{basevardir} || '/var/lib/ocsinventory-agent';
-    my $output_dir = $base_dir . '/update';
-    $logger->debug("[update] Computed output directory: $output_dir");
-
-    # Ensure directory exists
-    eval { make_path($output_dir) };
-    if ($@) {
-        $logger->error("Failed to create directory $output_dir: $@");
-        return 0;
-    }
-
-    my $info_path = $output_dir . '/info';
-    $logger->debug("[update] Writing update info to: $info_path");
+    
+    # Use the directory already created in prolog_reader
+    my $info_path = $context->{installpath} . '/update/info';
+    $logger->debug("Writing update info to: $info_path");
 
     my $data = {
         UPDATE => {
@@ -117,14 +108,34 @@ sub _write_update_info_xml {
     }
 }
 
+# At the beginning of end handler - implements locking mechanism
+sub begin {
+    my ($pidfile, $logger) = @_;
+
+    open LOCK_R, "$pidfile" or die("Cannot open pid file: $!");
+    if (flock(LOCK_R, LOCK_EX|LOCK_NB)) {
+        open LOCK_W, ">$pidfile" or die("Cannot open pid file: $!");
+        select(LOCK_W) and $|=1;
+        select(STDOUT) and $|=1;
+        print LOCK_W $$;
+        $logger->info("Beginning work. I am $$.");
+        return 0;
+    } else {
+        close(LOCK_R);
+        $logger->error("$pidfile locked. Cannot begin work... :-(");
+        return 1;
+    }
+}
+
 sub update_prolog_reader {
     my ($self, $prolog_raw_xml) = @_;
 
     my $logger  = $self->{logger};
     my $context = $self->{context};
     my $config  = $context->{config};
+    my $network = $context->{network};
 
-    $logger->debug('[update] Entering update_prolog_reader');
+    $logger->debug('Entering update_prolog_reader');
     # Parse prolog XML; be resilient to variations
     my $prolog;
     eval {
@@ -136,29 +147,49 @@ sub update_prolog_reader {
     }
 
     my $options = $prolog->{OPTION} || [];
-    $logger->debug('[update] Prolog OPTION count: ' . scalar(@{$options}));
+    $logger->debug('Prolog OPTION count: ' . scalar(@{$options}));
     my $update_params;
 
     foreach my $opt (@{$options}) {
         my $name = $opt->{NAME} || '';
         # Strict: only accept NAME == UPDATE (case-sensitive)
         if (defined $name && $name eq 'UPDATE') {
-            $logger->debug('[update] Found OPTION NAME=UPDATE');
+            $logger->debug('Found OPTION NAME=UPDATE');
             $update_params = _extract_update_from_option_params($opt->{PARAM});
             last;
         }
     }
 
+    # Create working directory and initialize files
+    my $opt_dir = $context->{installpath} . '/update';
+    
+    # Create working directory if it doesn't exist
+    if (!-d $opt_dir) {
+        mkdir($opt_dir) or do {
+            $logger->error("Cannot create $opt_dir: $!");
+            return;
+        };
+    }
+    
+    # Create lock file if needed
+    unless(-e "$opt_dir/lock") {
+        open LOCK, ">$opt_dir/lock" or do {
+            $logger->error("Cannot create lock file: $!");
+            return;
+        };
+        close(LOCK);
+    }
+
     # If no update option found, nothing to do
     unless ($update_params) {
-        $logger->debug("[update] No update option found in prolog; skipping update config generation");
+        $logger->debug("No update option found in prolog; skipping update config generation");
         return;
     }
 
     # Require at least URL_SUFFIX and VERSION to consider update available
     my $has_url     = defined $update_params->{URL_SUFFIX} && $update_params->{URL_SUFFIX} ne '';
     my $has_version = defined $update_params->{VERSION}    && $update_params->{VERSION}    ne '';
-    $logger->debug('[update] Extracted params - URL_SUFFIX: ' . (defined $update_params->{URL_SUFFIX} ? 'set' : 'unset')
+    $logger->debug('Extracted params - URL_SUFFIX: ' . (defined $update_params->{URL_SUFFIX} ? 'set' : 'unset')
                   . ', VERSION: ' . (defined $update_params->{VERSION} ? $update_params->{VERSION} : 'unset')
                   . ', ARGS: ' . (defined $update_params->{ARGS} ? $update_params->{ARGS} : 'unset')
                   . ', FORCE_UPDATE: ' . (defined $update_params->{FORCE_UPDATE} ? $update_params->{FORCE_UPDATE} : 'unset'));
@@ -167,8 +198,161 @@ sub update_prolog_reader {
         return;
     }
 
-    # Write using internalized output path
     _write_update_info_xml($self, $update_params);
+}
+
+# Asynchronous end handler for update module
+sub update_end_handler {
+    my ($self) = @_;
+    my $logger  = $self->{logger};
+    my $context = $self->{context};
+    
+    # Define working directory and info file for update module
+    my $opt_dir  = $context->{installpath} . '/update';
+    my $info_file = "$opt_dir/info";
+    my $pidfile  = "$opt_dir/lock";
+    
+    # Compute expected checksum in the parent process using MD5
+    open(my $fh, '<', $info_file) or do {
+        $logger->error("Cannot open info file $info_file");
+        return;
+    };
+    my $ctx = Digest::MD5->new;
+    $ctx->addfile($fh);
+    my $expected_checksum = $ctx->hexdigest;
+    close($fh);
+    
+    # Fork a new process
+    my $pid = fork();
+    if (!defined $pid || $pid < 0) {
+        $logger->error("Failed to fork child process <ret:$pid> <err:$!>");
+        return;
+    } elsif ($pid > 0) {
+        $logger->info("Child process forked successfully <pid:$pid>");
+        return;  # Parent process exits here
+    }
+    
+    # Child process: Set up signal handler
+    $logger->debug("Setting up signal handler for USR1");
+    $SIG{'USR1'} = sub {
+        $logger->info("received USR1 signal - finishing");
+        finish($logger, $context);
+    };
+
+    # Change to the update working directory
+    chdir($opt_dir) or die("Cannot chdir to working directory...Abort\n");
+
+    # Acquire lock before proceeding
+    $logger->debug("Trying to acquire lock");
+    if (begin($pidfile, $logger)) {
+        exit(0);
+    }
+
+    # Execute core task
+    $logger->debug("initiating core update task");
+    $self->_update_core_task($expected_checksum);
+
+    # Clean exit through finish
+    $logger->debug("initiating cleanup");
+    finish($logger, $context);
+}
+
+# Private subroutine containing only the core validation logic
+sub _update_core_task {
+    my ($self, $expected_checksum) = @_;
+    my $logger  = $self->{logger};
+    my $context = $self->{context};
+    my $network = $context->{network};
+
+    my $info_file = $context->{installpath} . '/update/info';
+
+    # Validate checksum and read info file
+    $logger->debug("Opening info file for checksum <$info_file>");
+    open(my $fh_child, '<', $info_file) or do {
+        $logger->error("Failed to open info file for checksum <$info_file>");
+        return 0;
+    };
+    
+    my $child_ctx = Digest::MD5->new;
+    $child_ctx->addfile($fh_child);
+    my $child_checksum = $child_ctx->hexdigest;
+    close($fh_child);
+
+    $logger->debug("Validating checksum: expected <$expected_checksum>, got <$child_checksum>");
+    if ($child_checksum eq $expected_checksum) {
+        $logger->debug("Checksum matches, proceeding to read info file");
+        
+        # Read the info file
+        $logger->debug("Reading info file <$info_file>");
+        my $info = XML::Simple::XMLin("$info_file");
+        $logger->debug(sprintf("Update config: Version=%s, URLSuffix=%s, Args=%s, Force=%s",
+            $info->{VERSION},
+            $info->{URL_SUFFIX},
+            $info->{ARGS},
+            $info->{FORCE_UPDATE}));
+
+        # Check if update is required
+        my $current_version = version->parse($context->{config}->{itam_version} || '0.0.0');
+        my $availabe_version = version->parse($info->{VERSION} || '0.0.0');
+        my $force_update = $info->{FORCE_UPDATE} || 0;
+        
+        # Compare versions and check force flag
+        if (!$force_update && $current_version >= $info->{VERSION}) {
+            $logger->info("Update not required: Current version ($current_version) >= Available version ($info->{VERSION})");
+            return 1;
+        }
+        
+        $logger->info(sprintf("Update required: %s (current: %s -> available: %s)", 
+            $force_update ? "Force update enabled" : "New version available",
+            $current_version,
+            $info->{VERSION}
+        ));
+
+        # Download the update
+        unless (defined $info->{URL_SUFFIX} && $info->{URL_SUFFIX} ne '') {
+            $logger->error("No URL_SUFFIX provided in info file, cannot proceed with update");
+            return 0;
+        }
+        
+        my $max_tries = 5;  # Default max tries for download
+        my $full_url = "https://item-agents.s3.us-east-1.amazonaws.com/" . $info->{URL_SUFFIX};
+        
+        # Choose installer extension based on OS
+        my $installer_ext = $^O eq 'darwin' ? '.pkg' : '.deb';
+        my $installer_path = $context->{installpath} . '/update/installer' . $installer_ext;
+        $logger->debug("Downloading update from: $full_url");
+        my $err_msg;
+        for (my $try = 1; $try <= $max_tries; $try++)
+        {
+            $err_msg = $network->getFileFromUrl($full_url, $installer_path);
+            last unless defined($err_msg); # break early if not failed
+        }
+
+        # In case of failure
+        if (defined($err_msg)) {
+            $logger->error("Failed to download installer in maximum tries <error:$err_msg> <tries:$max_tries>");
+            # report error and clean package in case URL is expired
+            if ($err_msg eq 'Request has expired') {
+                $logger->info("Failed to download installer because URL has expired");
+            }
+            return 1;
+        }
+
+        $logger->debug("Installer downloaded successfully");
+        return 1;
+    } else {
+        $logger->error("Checksum mismatch for info file. Expected $expected_checksum but got $child_checksum");
+        return 0;
+    }
+}
+
+# Private finish subroutine for update module - mirrors the download finish
+sub finish {
+    my ($logger, $context) = @_;
+    # Open or create the lock file in update directory
+    open my $LOCK, '>', $context->{installpath} . '/update/lock' or warn "Cannot open lock file\n";
+    $logger->debug("End of work...\n");
+    exit(0);
 }
 
 1;
--- lib/Ocsinventory/LoggerBackend/File.pm
+++ lib/Ocsinventory/LoggerBackend/File.pm
@@ -17,12 +17,13 @@ sub addMsg {
 
     my $level = $args->{level};
     my $message = $args->{message};
+    my $ppid = getppid();  # Get parent process ID for logging
 
     return if $message =~ /^$/;
 
     open FILE, ">>".$self->{config}->{logfile} or warn "Can't open ".
     "`".$self->{config}->{logfile}."'\n";
-    print FILE "[".localtime()."][$level] $message\n";
+    print FILE "[".localtime()."][$ppid:$$][$level] $message\n";
     close FILE;
 
 }
